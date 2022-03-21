a5dca705625c71fd718e7ed1d92720119effd487
artifact911 <artifact911@gmail.com>
2022-03-21 21:10:41 +0300

    first commit

---
+++ src/test/java/org/example/AppTest.java
@@ -0,0 +1,15 @@
+package org.example;
+
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertTrue;
+
+public class AppTest {
+
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
