c12b5301817af4bcd692285bbbd83a5bca257310
artifact911 <artifact911@gmail.com>
2022-03-21 23:13:14 +0300

    fix one

--- src/test/java/org/example/SecondTest.java
+++ src/test/java/org/example/SecondTest.java
@@ -1,2 +1,30 @@
-package org.example;public class SecondTest {
+package org.example;
+
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class SecondTest {
+
+    private WebDriver driver;
+
+    @BeforeClass
+    public void setUp() {
+        System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver");
+        driver = new ChromeDriver();
+    }
+
+    @AfterClass
+    public void finish() {
+        driver.quit();
+    }
+
+    @Test
+    public void openGoogle() {
+        driver.get("https://www.google.com");
+    }
 }
+
