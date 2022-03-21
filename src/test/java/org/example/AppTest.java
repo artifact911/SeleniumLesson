783d98d61e54d136cc851bf74bce5cdcb62490c9
artifact911 <artifact911@gmail.com>
2022-03-21 21:26:21 +0300

    second commit

--- src/test/java/org/example/AppTest.java
+++ src/test/java/org/example/AppTest.java
@@ -3,6 +3,7 @@ package org.example;
 
 import org.testng.annotations.Test;
 
+import static org.testng.Assert.assertFalse;
 import static org.testng.Assert.assertTrue;
 
 public class AppTest {
@@ -11,5 +12,10 @@ public class AppTest {
     public void shouldAnswerWithTrue()
     {
         assertTrue( true );
+    }
+
+    @Test
+    public void shouldAnswerFalse() {
+        assertFalse(false);
     }
 }
